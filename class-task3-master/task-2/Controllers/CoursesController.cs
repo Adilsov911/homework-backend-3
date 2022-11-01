7ea85c14d8c3c84f8b255d9d9bbe76664f3139a6
Adilsov911 <adiles@code.edu.az>
2022-11-01 23:22:56 +0400

    Task Home

---
+++ class-task3-master/task-2/Controllers/CoursesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace task_2.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
